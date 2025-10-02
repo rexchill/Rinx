66298a02776c32ac5f83878fc70ed44b50deb8f2
rexchill <rexshelln@gmail.com>
2025-10-02 22:35:04 +0800

    feat: 读写分离粗略实现

---
+++ Demo/v0.7/Client0.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"Rinx/rnet"
+	"fmt"
+	"io"
+	"net"
+	"time"
+)
+
+func main() {
+	fmt.Println("客户端开始连接...")
+	time.Sleep(time.Second)
+	// 连接到服务器
+	conn, err := net.Dial("tcp4", "127.0.0.1:19991")
+	if err != nil {
+		fmt.Println("连接失败...", err)
+		return
+	}
+	for {
+		// 连接成功
+		// 封包
+		pkg := rnet.NewDataPackage()
+		msgSend := rnet.NewMessage(0, []byte("Client0..."))
+		binaryMsg, err := pkg.Pack(msgSend)
+		if err != nil {
+			fmt.Println("客户端封装数据失败...", err)
+			continue
+		}
+
+		// 发送
+		if _, err := conn.Write(binaryMsg); err != nil {
+			fmt.Println("发送数据失败...", err)
+			continue
+		}
+
+		// 读取数据头部
+		header := make([]byte, pkg.GetHeadLen())
+		if _, err := io.ReadFull(conn, header); err != nil {
+			fmt.Println("读取数据头部失败...", err)
+			continue
+		}
+		// 解析数据头部
+		msgRecv, err := pkg.UnPack(header)
+		if err != nil {
+			fmt.Println("解包数据头部失败...", err)
+			continue
+		}
+
+		// 读取数据内容
+		body := make([]byte, msgRecv.GetDataLen())
+		if _, err := io.ReadFull(conn, body); err != nil {
+			fmt.Println("读取数据内容失败...", err)
+			continue
+		}
+		// 解析数据内容
+		msgRecv.SetMsgData(body)
+
+		// 接受数据成功，打印验证
+		fmt.Println("==> Recv Msg: ID=", msgRecv.GetMsgId(), ", len=", msgRecv.GetDataLen(), ", data=", string(msgRecv.GetMsgData()))
+		time.Sleep(1 * time.Second)
+	}
+}
--- rnet/connection.go
+++ rnet/connection.go
@@ -23,8 +23,11 @@ type Connection struct {
 	// 当前连接消息类型对应的所要执行的业务方法
 	MsgHandler riface.IMsgHandler
 
-	// 用于通知当前连接已经退出的 channel
+	// 用于通知当前连接已经退出的 channel；读协程获取到从客户端发来的退出信号，通知写协程退出
 	ExitChan chan struct{}
+
+	// 用于读写协程间的通信
+	msgChan chan []byte
 }
 
 // NewConnection 初始化连接模块
@@ -35,13 +38,14 @@ func NewConnection(conn *net.TCPConn, connID uint32, msgHandler riface.IMsgHandl
 		isClosed:   false,
 		MsgHandler: msgHandler,
 		ExitChan:   make(chan struct{}),
+		msgChan:    make(chan []byte),
 	}
 	return con
 }
 func (conn *Connection) StartReader() {
-	fmt.Println("读业务协程开始工作...")
-	defer fmt.Println("读业务结束...连接ID为：", conn.ConnID, "远程客户端地址为： ", conn.RemoteAddr().String())
+	fmt.Println("[读业务协程开始工作...]")
 	defer conn.Stop()
+	defer fmt.Println("读业务结束...连接ID为：", conn.ConnID, "远程客户端地址为： ", conn.RemoteAddr().String())
 
 	// 开始处理业务
 	for {
@@ -60,6 +64,7 @@ func (conn *Connection) StartReader() {
 		msg, err := pkg.UnPack(header)
 		if err != nil {
 			fmt.Println("message头部解析失败...", err)
+			return
 		}
 
 		// 根据头部字段中的长度读取对应的数据
@@ -84,10 +89,30 @@ func (conn *Connection) StartReader() {
 	}
 }
 
+func (conn *Connection) StartWriter() {
+	fmt.Println("[写业务协程开始工作...]")
+	defer fmt.Println("[写协程退出...], 客户端地址为：", conn.RemoteAddr().String())
+	for {
+		select {
+		case data := <-conn.msgChan:
+			// TODO 应该在写协程进行业务的处理（？）
+			// 接收读业务进行业务处理后的数据
+			if _, err := conn.GetTCPConnection().Write(data); err != nil {
+				fmt.Println("[写协程发送数据失败...]", err)
+				return
+			}
+		case <-conn.ExitChan:
+			// 客户端退出连接，结束写业务
+			return
+		}
+	}
+
+}
+
 func (conn *Connection) SendMsg(msgId uint32, msgData []byte) error {
 	// 连接关闭，给出关闭信息并返回
 	if conn.isClosed {
-		return errors.New("连接已关闭...")
+		return errors.New("[连接已关闭...]")
 	}
 
 	// 封包
@@ -97,11 +122,8 @@ func (conn *Connection) SendMsg(msgId uint32, msgData []byte) error {
 		return err
 	}
 
-	// 发送给客户端
-	_, err = conn.GetTCPConnection().Write(binaryMsg)
-	if err != nil {
-		fmt.Println("发送二进制数据失败...", err)
-	}
+	// 通过管道发给写业务
+	conn.msgChan <- binaryMsg
 
 	return nil
 }
@@ -109,11 +131,27 @@ func (conn *Connection) Start() {
 	fmt.Println("连接开始...连接ID为： ", conn.ConnID)
 	// 启动该连接的读业务
 	go conn.StartReader()
-	// TODO 写业务
+	// 启动该连接的写业务
+	go conn.StartWriter()
 }
 
 func (conn *Connection) Stop() {
-
+	fmt.Println("[执行Stop...]")
+	// 连接已经关闭
+	if conn.isClosed {
+		return
+	}
+	// 否则，置关闭标识
+	conn.isClosed = true
+	// 服务端关闭连接
+	if err := conn.GetTCPConnection().Close(); err != nil {
+		fmt.Println("[服务端关闭连接失败...]", err)
+	}
+	// 告知Writer连接已关闭
+	conn.ExitChan <- struct{}{}
+	// 回收资源
+	close(conn.ExitChan)
+	close(conn.msgChan)
 }
 
 func (conn *Connection) GetTCPConnection() *net.TCPConn {
